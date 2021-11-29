2d11020bb3a5980ea21782941bf342d10e2b7111
Haleem Hamid <hhamidbiz@gmail.com>
2021-11-29 11:02:12 -0800

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var employee = {
+    name: "Haleem",
+    streetAddress:"Crenshaw",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmp = {...employee};
+    newEmp[key] = value;
+    return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newRoster = {...employee}
+    delete newRoster[key];
+    return newRoster;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
